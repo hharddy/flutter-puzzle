3590e6ba99df9a54ad08638dc70fee5b79875c5e
hharddy <hharddy@yahoo.com>
2023-08-25 22:58:21 +0330

    creating game panel

--- lib/item/game_item.dart
+++ lib/item/game_item.dart
@@ -8,20 +8,21 @@ class GameItem extends StatelessWidget {
 
   GameItem(this.gameModel){
 
-    switch(gameModel.pieces!){
-      case PIECES.NOTHING: print("Nothing");
-      case PIECES.INIT: print("INIT");
-      case PIECES.BOMB: print("BOMB");
-      case PIECES.GEM: print("GEM");
-    //  case null: print("null");
 
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
+      // constraints: BoxConstraints(
+      //   minWidth: 30,
+      //   maxWidth: 10,
+      //   maxHeight: 20,
+      //   minHeight: 20
+      // ),
+      height: 60,
+      width: 60,
       color: () {
         switch(gameModel.pieces!){
           case PIECES.NOTHING: return Colors.yellow;
@@ -30,12 +31,10 @@ class GameItem extends StatelessWidget {
           case PIECES.GEM: return Colors.yellow;
         }
       }(),
-      height: 15,
-      width: 15,
       child: () {
        switch(gameModel.pieces!){
          case PIECES.NOTHING: return Image.asset('/icon/bomb3.png',fit: BoxFit.fill,);
-         case PIECES.INIT: return Image.asset('/icon/bomb3.png',fit: BoxFit.fill,);
+         case PIECES.INIT: return Image.asset('/icon/bomb2.png',fit: BoxFit.fill,);
          case PIECES.BOMB: return Image.asset('/icon/bomb3.png',fit: BoxFit.fill,);
          case PIECES.GEM: return Image.asset('/icon/gem.png',fit: BoxFit.fill,);
        }
--- lib/widget/game_panel.dart
+++ lib/widget/game_panel.dart
@@ -17,22 +17,38 @@ class _GamePanelState extends State<GamePanel> {
   List<GameModel> game_peices = [
 
     GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
-    GameModel(id: 1,choosed: false),
+    GameModel(id: 2,choosed: false),
+    GameModel(id: 3,choosed: false),
+    GameModel(id: 4,choosed: false),
+    GameModel(id: 5,choosed: false),
+    GameModel(id: 6,choosed: false),
+    GameModel(id: 7,choosed: false),
+    GameModel(id: 8,choosed: false),
+    GameModel(id: 9,choosed: false),
+    GameModel(id: 10,choosed: false),
+    GameModel(id: 11,choosed: false),
+    GameModel(id: 12,choosed: false),
+    GameModel(id: 13,choosed: false),
+    GameModel(id: 14,choosed: false),
+    GameModel(id: 15,choosed: false),
+    GameModel(id: 16,choosed: false),
+    GameModel(id: 17,choosed: false),
+    GameModel(id: 18,choosed: false),
+    GameModel(id: 19,choosed: false),
+    GameModel(id: 20,choosed: false),
+    GameModel(id: 21,choosed: false),
+    GameModel(id: 22,choosed: false),
+    GameModel(id: 23,choosed: false),
+    GameModel(id: 24,choosed: false),
   ];
+  double _height = 450;
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          margin: EdgeInsets.only(right: MediaQuery.of(context).size.width*0.03,top: 15),
-          height: 420,
+          margin: EdgeInsets.only(right: 20, top: 15),
+          height: _height,
           width: MediaQuery.of(context).size.width,
           // color: Colors.green,
           decoration: BoxDecoration(
@@ -52,35 +68,63 @@ class _GamePanelState extends State<GamePanel> {
           ),
         ),
         Container(
-          margin: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.03, top: 15),
-          height: 420,
+          margin: EdgeInsets.only(right: 20, top: 15),
+          height: _height,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(main_radius),
             color: Palette.primary.withOpacity(0.7),
           ),
           child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 35,vertical: 10),
-            child: GridView.builder(
+            margin: EdgeInsets.symmetric(horizontal: 5,vertical: 10),
+            height: 400,
+            child: Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 45,
+                      height: 45,
+                      child: Image.asset('/icon/bomb2.png',fit: BoxFit.fill,),
+                    ),
+                    Text("Bomb",style: TextStyle(color: Colors.white),),
+                  ],
+                ),
+                Expanded(
+                  child: Container(
+                  //  height: 400,
+                   // width: 200,
+                    color: Colors.red,
+                    child: ListView.builder(
+                      itemCount: (game_peices.length + 3) ~/ 4, // Calculate the number of rows needed
+                      itemBuilder: (context, rowIndex) {
+                        int startingIndex = rowIndex * 4;
+                        int endingIndex = startingIndex + 3;
+                        endingIndex = endingIndex < game_peices.length ? endingIndex : game_peices.length - 1;
 
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 15,
-                 crossAxisSpacing: 20,
-               //  mainAxisExtent: 60,
-                childAspectRatio: 1.4
-                // Number of columns in the grid
-              ),
-              itemCount: game_peices.length,
-              itemBuilder: (context, index) {
+                        List<Widget> rowItems = [];
+                        for (int index = startingIndex; index <= endingIndex; index++) {
+                          rowItems.add(
 
-                return Container(
-                  margin: EdgeInsets.only(top: 4),
+                            //    padding: EdgeInsets.all(8.0), // Add padding between items
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: GameItem(game_peices[index]),
+                            ),
 
-                 color: Colors.green,
-                  child: GameItem(game_peices[index]),
-                );
-              },
+                          );
+                        }
+
+                        return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: rowItems);
+                      },
+                    ),
+                  ),
+                ),
+                Text("dddddddddddd",style: TextStyle(color: Colors.white),),
+              ],
             ),
           ),
         )
